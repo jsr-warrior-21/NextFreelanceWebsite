27deed007b20cfb8a2526bbbfef6f1b928791d27
Arvind <jsrarvind12@gmail.com>
2026-08-25 00:42:59 +0530

    implemented checking of unique username in frontend in sign-in page

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,63 @@
-export default function Page(){
-    return (
-        <div>
-            <h1>Page</h1>
-        </div>
-    )
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
+import { useDebounceValue } from "usehooks-ts";
+import { toast } from "sonner";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { signUpSchema } from "@/schemas/signUpSchema";
+import axios, { AxiosError } from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
+
+export default function Page() {
+  const [username, setUsername] = useState("");
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const debouncedUsername = useDebounceValue(username, 300);
+
+  const router = useRouter();
+
+  const form = useForm({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  useEffect(() => {
+    const checkUsernameUnique = async () => {
+      if (debouncedUsername) {
+        setIsCheckingUsername(true);
+        setUsernameMessage("");
+      }
+
+      try {
+        const response = await axios.get(
+          `/api/check-username-unique?username=${debouncedUsername}`,
+        );
+        setUsernameMessage(response.data?.message);
+      } catch (error) {
+        const axiosError = error as AxiosError<ApiResponse>;
+        setUsernameMessage(
+          axiosError.response?.data?.message ?? "Error checking username.",
+        );
+      } finally {
+        setIsCheckingUsername(false);
+      }
+    };
+    checkUsernameUnique();
+  }, [debouncedUsername]);
+
+  return (
+    <div>
+      <h1>Page</h1>
+    </div>
+  );
 }
