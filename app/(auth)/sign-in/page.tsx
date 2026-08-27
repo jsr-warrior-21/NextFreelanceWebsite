4e59fc25936ae792b435178697ecdd6ee5c3ad75
Arvind <jsrarvind12@gmail.com>
2026-08-28 01:09:05 +0530

    done with sign up with auth

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,89 @@
-export default function Signin(){
-  return(
-    <div>
-      
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { toast } from "@/components/ui/toast";
+import { useRouter } from "next/navigation";
+import { Controller } from "react-hook-form";
+import { Input } from "@/components/ui/input";
+import { Field, FieldLabel, FieldError } from "@/components/ui/field";
+import { Button } from "@/components/ui/button";
+import { signInScheme } from "@/schemas/signInSchema";
+import { signIn } from "next-auth/react";
+
+export default function Page() {
+
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof signInScheme>>({
+    resolver: zodResolver(signInScheme),
+    defaultValues: {
+      identifier: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof signInScheme>) => {
+    const result = await signIn("credentials", {
+      redirect: false,
+      identifier: data.identifier,
+      password: data.password,
+    });
+
+    if (result?.error) {
+      toast.add({
+        title: "Login Failed",
+        description: "Incorrect username or password.",
+      });
+    }
+
+    if (result?.url) {
+      router.replace("/dashboard");
+    }
+  };
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Join Mystery Message
+          </h1>
+          <p className="mb-4">Sign in to start your anonymous adventure</p>
+        </div>
+
+        {/* form start here */}
+
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <Controller
+            name="identifier"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={!!fieldState.error || undefined}>
+                <FieldLabel>Email/Username</FieldLabel>
+                <Input type="email" placeholder="email/username" {...field} />
+                <FieldError>{fieldState.error?.message}</FieldError>
+              </Field>
+            )}
+          />
+
+          <Controller
+            name="password"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={!!fieldState.error || undefined}>
+                <FieldLabel>Password</FieldLabel>
+                <Input type="password" placeholder="password" {...field} />
+                <FieldError>{fieldState.error?.message}</FieldError>
+              </Field>
+            )}
+          />
+
+          <Button type="submit" >
+              Signin
+          </Button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
